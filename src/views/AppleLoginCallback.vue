0d2a9c1777e6ec81fb28e8d15b9bc6e4245ef8b8
yanghyesun89 <ssafy0507@gmail.com>
2024-11-02 17:39:16 +0900

    [init] init

--- src/views/AppleLoginCallback.vue
+++ src/views/AppleLoginCallback.vue
@@ -2,8 +2,37 @@
   <div></div>
 </template>
 <script>
+import { postLogin } from "@/api/auth.js";
+const appleState = process.env.VUE_APP_APPLE_LOGIN_STATE;
 export default {
   name: "AppleLoginCallback",
+  mounted() {
+    document.addEventListener("AppleIDSignInOnSuccess", (data) => {
+      let state = data.detail.authorization.state;
+      if (state === appleState) {
+        let authId = data.detail.authorization.id_token;
+        postLogin(authId, "apple").then((result) => {
+          const status = data.status;
+          console.log(status);
+          if (status === 200) {
+            const data = result.data;
+            sessionStorage.setItem("loginType", "apple");
+            sessionStorage.setItem("appleIdToken", authId);
+            sessionStorage.setItem("accessToken", data.data.accessToken);
+            sessionStorage.setItem("refreshToken", data.data.refreshToken);
+            sessionStorage.setItem("email", data.data.email);
+            this.$router.push("home");
+          } else {
+            alert(this.$i18n.t("error"));
+          }
+        });
+      }
+    });
+    document.addEventListener("AppleIDSignInOnFailure", (error) => {
+      console.log(error);
+      alert(this.$i18n.t("apple_login_error"));
+    });
+  },
 };
 </script>
 <style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -42,8 +42,6 @@ const appleClientId = process.env.VUE_APP_APPLE_LOGIN_CLIENT_ID;
 const appleState = process.env.VUE_APP_APPLE_LOGIN_STATE;
 const domain = "https://catchlink.meomimo.com";
 
-import { postLogin } from "@/api/auth.js";
-
 export default {
   name: "LoginView",
   data() {
@@ -72,31 +70,6 @@ export default {
       state: appleState,
       nonce: "nonce",
       usePopup: false,
-    });
-    document.addEventListener("AppleIDSignInOnSuccess", (data) => {
-      let state = data.detail.authorization.state;
-      if (state === appleState) {
-        let authId = data.detail.authorization.id_token;
-        postLogin(authId, "apple").then((result) => {
-          const status = data.status;
-          console.log(status);
-          if (status === 200) {
-            const data = result.data;
-            sessionStorage.setItem("loginType", "apple");
-            sessionStorage.setItem("appleIdToken", authId);
-            sessionStorage.setItem("accessToken", data.data.accessToken);
-            sessionStorage.setItem("refreshToken", data.data.refreshToken);
-            sessionStorage.setItem("email", data.data.email);
-            this.$router.push("home");
-          } else {
-            alert(this.$i18n.t("error"));
-          }
-        });
-      }
-    });
-    document.addEventListener("AppleIDSignInOnFailure", (error) => {
-      console.log(error);
-      alert(this.$i18n.t("apple_login_error"));
     });
   },
   methods: {
